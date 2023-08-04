0679cc031d66557ec6d7a34ca9cfbd4fa35eecf3
Festus Omaku <fomaku3@gmail.com>
2023-08-04 14:31:40 +0100

    execute file

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python3
-"""
-This module contains a function that calculates the sum of two numbers.
-"""
+"""This module contains a function that calculates the sum of two numbers."""
 
 
 def sum(a: float, b: float) -> float:
-    """
-    Calculate the sum of two numbers.
-
-    Args:
-        x (float): The first number.
-        y (float): The second number.
-
-    Returns:
-        float: The sum of the two numbers.
-    """
+    """ Calculate the sum of two numbers. """
 
     return a + b
