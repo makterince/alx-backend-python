a666d336eff0b6f705e5ec2fb1ba9285da9e2484
Festus Omaku <fomaku3@gmail.com>
2023-08-04 14:33:23 +0100

    fix

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,7 +2,7 @@
 """This module contains a function that calculates the sum of two numbers."""
 
 
-def sum(a: float, b: float) -> float:
+def add(a: float, b: float) -> float:
     """ Calculate the sum of two numbers. """
 
     return a + b
