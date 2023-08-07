a026bf9b15c052847a79c7dd27ffbe67220e53fc
Festus Omaku <fomaku3@gmail.com>
2023-08-08 00:44:16 +0100

    task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -4,7 +4,7 @@
 
 import asyncio
 from typing import Task
-from random_delay import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> Task:
