82977889470a7686929c44f93aed45c96134870c
Festus Omaku <fomaku3@gmail.com>
2023-08-07 23:04:36 +0100

    asunc task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" this module waits a random delay and returns its argument """
+
+
+import asyncio
+import random
+
+async def wait_random(max_delay=10):
+    """     Waits for a random delay between 0 and max_delay seconds """
+
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
