14e9ffa70cc141a3a86a4e3190e05805e0b30e18
Festus Omaku <fomaku3@gmail.com>
2023-08-08 00:32:20 +0100

    fix

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -4,16 +4,16 @@
 
 import time
 import asyncio
-from typing import Callable
+
+
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int, func: Callable) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """ Measures the average execution time for a given function """
 
-     start_time = time.time()
-     asyncio.run(wait_n(n, max_delay))
-     end_time = time.time()
-     total_time = end_time - start_time
-     average_time = total_time / n
-     return average_time
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.time() - start_time
+    average_time = total_time / n
+    return average_time
